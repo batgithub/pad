82e29184a7ffdf7a6f9d34f70f2e74ecaa7482d1
Baptiste <lhopitault.baptiste@gmail.com>
2016-04-17 23:07:46 +0200

    test recupération de la durée

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 function loader() {
-    $("button").each(function(){
-        var file = $(this).data('file');
-        $(this).append(
-            '<audio preload="auto" id="'+file +'"><source src="audio/'+ file +'.mp3"></audio >'
-        )
-    })
+    $("button").each(
+        function(){
+            var file = $(this).data('file');
+
+            var audio = '<audio preload="auto" id="'+file +'"><source src="audio/'+ file +'.mp3"><span>'+ +'</span></audio>';
+
+            var dure = '<span>'+ $('#'+ file)+'</span>';
+
+            console.log( $('#'+ file) );
+
+            $(this).append(audio,dure)
+        }
+
+    )
 }
 
+
+
 function playSound(id) {
-    var audioClick = document.getElementById(id);
+    var audioClick = $('#'+ id)[0];
+    console.log($('#'+ id));
     audioClick.play();
 }
